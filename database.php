9e5ceb41a3a5ba2621afacc6baaceb8bae8b1654
Parvathy Vijayan <99131599+PARVATHY-VIJAYAN@users.noreply.github.com>
2023-06-27 07:42:33 +0530

    database connection
    database connection for all the users are kept outside because every user is access the same database named 'classcommitteedb'. Which will be provided as classcommitteedb.sql

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+    $host="localhost";
+    $username="root";
+    $password="";
+    $database="classcommitteedb";
+    $connection = mysqli_connect($host, $username, $password,$database);
+?>
